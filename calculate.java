eadf64eb5941affcec0c66baebfd05f26fbd2062
Vinitha-02 <vinitha.k@marketsimplified.com>
2022-04-27 16:15:29 +0530

    changed

--- calculate.java
+++ calculate.java
@@ -1,47 +1,45 @@
-public class calculate {
-    int height;
+public class calculate extends shape {
+
+    public static void main(String[] args) {
+        Rectangle s1 = new Rectangle();
+        s1.find1(5, 2);
+        Circle s2 = new Circle();
+        s2.find2(10);
+        Square s3 = new Square();
+        s3.find3(4);
+    }
+
+}
+
+class shape {
+    int length;
     int width;
     int radius;
     double pi = 3.14;
     double side;
+}
 
-    public class Rectangle extends calculate
-    public void calculate(int l, int w)
-    {
-    length=l;
-    width=w;
-    int area = length* width;
-    System.out.println("Area of the rectangle is" +area);
-    }
-    }
-
-    public class Circle extends Shape {
-        public void Calculate(int r) {
-            r = radius;
-            int area = pi * radius * radius;
-            System.out.println("Area of the square is" + area);
-        }
-    }
-
-    public class Square extends Shape {
-        public void Calculate(double s) {
-            side = s;
-            int area = side * side;
-            System.out.println("Area of the square is" + area);
-        }
+class Rectangle extends shape {
+    void find1(int l, int w) {
+        length = l;
+        width = w;
+        int area = length * width;
+        System.out.println("Area of the rectangle is" + area);
     }
 }
 
-public class Area1()
-{
+class Circle extends shape {
+    public void find2(int r) {
+        r = radius;
+        double area = pi * radius * radius;
+        System.out.println("Area of the square is" + area);
+    }
+}
 
-    public static void main (String args[])
-    {
-    Rectangle s1 = new Rectangle();
-    s1.calculate(5,2);
-    Circle s2 = new Circle();
-    s2.calculate(10);
-    Square s3 = new Square();
-    s3.calculate(3);
+class Square extends shape {
+    public void find3(double s) {
+        side = s;
+        double area = side * side;
+        System.out.println("Area of the square is" + area);
     }
 }
